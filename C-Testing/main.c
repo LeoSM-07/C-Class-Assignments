7f7d5502407f1dec0b69f69be55c2f4caca0111a
LeoSM_07 <leosmindlin@icloud.com>
2022-09-19 11:13:45 -0700

    Initial Commit

---
+++ C-Testing/main.c
@@ -0,0 +1,14 @@
+//
+// main.c
+// C-Testing
+//
+// Created by LeoSM_07 on 9/19/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
