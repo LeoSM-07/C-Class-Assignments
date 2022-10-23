e4bbc0576fde6b274c2a39b57041aa91de42f8e0
LeoSM_07 <leosmindlin@icloud.com>
2022-10-22 20:41:37 -0700

    Update

--- C-Testing/main.c
+++ C-Testing/main.c
@@ -1,132 +1,81 @@
 // Leo
 // CS 50
-// Project 3
+// Project 4
 // 10/22/2022
 
 #include <stdio.h>
-#include <time.h>           // for time() function
-#include <stdlib.h>         // for srand() and rand() functions
 
-#define ARRAY_SIZE 20       // Size of array
-#define UPPER_LIMIT 20      // Max. number of arrays used
-#define LOWER_LIMIT 12      // Min. number of arrays used
-#define DATA_UPPER_LIMIT 90 // Max. of each array element
-#define DATA_LOWER_LIMIT 10 // Min. of each array element
+void MultiplyDivide (double a, double b, double *c, double *d);
+void AmountAboveAverage (int array[20], int arrSize, int arrAverage, int *amountAboveAverage);
 
-void RandomizeArray(int array[], int arrSize);
-void PrintArrayValues(int array[], int arrSize);
-void FindMedianOfArray(int array[], int arrSize);
-void GetMinimumMaximum(int array[], int arrSize);
-void FindMedianOfArray(int array[], int arrSize);
+void MultiplyDivide (double a, double b, double *c, double *d)
 
-void SortArray(int array[], int arrSize);
-
-
-void PrintArrayValues(int array[], int arrSize) {
-    int index;
-    for(index = 0; index < arrSize; index++) {
-        printf("%d ", array[index]);
-    }
-
-    printf("\n");
+{
+    *c = a/b;
+    *d = a*b;
 }
 
-void GetMinimumMaximum(int array[], int arrSize) {
-    int index;
-    int maximum = array[0];
-    int minimum = array[0];
-    for(index = 0; index < arrSize; index++) {
-        int number = array[index];
-        if(number > maximum) {
-            maximum = number;
-        }
-        if(number < minimum) {
-            minimum = number;
+void AmountAboveAverage (int array[20], int arrSize, int arrAverage, int *amountAboveAverage)
+{
+    int i;
+    int total = 0;
+
+    printf("arrsize: %d \n", arrSize);
+    printf("average:%d \n", arrAverage );
+
+    for(i = 0; i < arrSize; i++) { // Check if each element is above the average
+        if (array[i] > arrAverage) {
+            total++;
         }
     }
-    printf("\nMinimum Int = %d\n", minimum);
-    printf("Maximum Int = %d\n", maximum);
+
+    *amountAboveAverage = total;
 }
 
-void FindArrayAverage(int array[], int size) {
-    int totalSize;
-    int averageSize;
-    int index;
+int main (void) {
 
-    totalSize = 0;
-    for(index = 0; index < size; index++) {
-        totalSize+= array[index];
+    printf("Léo Mindlin - Project 4\n\n");
+
+// BEGIN PART 1:
+    double a = 3.5;
+    double b = 2.1;
+    double c;
+    double d;
+
+    printf("Enter value for a: ");
+    scanf("%lf", &a);
+    printf("Enter value for b: ");
+    scanf("%lf", &b);
+
+    MultiplyDivide(a, b, &c, &d);
+
+    printf("value of c: %f \nvalue of d: %f \n\n\n", c, d);
+
+
+// BEGIN PART 2:
+    int array[20];
+    int i;
+    int v;
+    int sum = 0;
+    int amountAboveAverage;
+
+
+    for (i = 0; i < 20; ++i) { // Get 20 inputs
+        printf("Value %d: ",i+1);
+        scanf("%d", &v);
+        array[i] = v;
     }
 
-    averageSize = totalSize / size;
-    printf("\nAverage = %d\n\n", averageSize);
-}
-
-void RandomizeArray(int array[], int size) {
-    int index;
-
-    for(index = 0; index < size; index++) {
-
-        int random = rand() % (DATA_UPPER_LIMIT - DATA_LOWER_LIMIT + 1) + DATA_LOWER_LIMIT;
-        array[index] = random;
-
-    }
-}
-
-void SortArray(int array[], int arrSize) { // Using a selection sort
-    int step;
-    for(step = 0; step < arrSize - 1; step++) {
-
-        int min = step;
-
-        int index;
-
-        for(index = step + 1; index < arrSize; index++) {
-            if(array[index] < array[min]) {
-                min = index;
-
-            }
-        }
-        int temp = array[step];
-        array[step] = array[min];
-        array[min] = temp;
-        PrintArrayValues(array, arrSize);
-    }
-}
-
-
-void FindMedianOfArray(int array[], int arrSize) {
-    int median;
-    if(arrSize % 2 == 0) {
-        int below = array[arrSize / 2 - 1];
-        int above = array[arrSize / 2];
-        median = (below + above) / 2;
-    }
-    else {
-        median = array[arrSize / 2];
+    for(i = 0; i < 20; i++) { // Find the sum of array
+       sum = sum + array[i];
     }
 
-    printf("\nMedian of array = %d\n", median);
-}
+    int average = sum/20;
 
-int main(void) {
-    srand(time(NULL));
-    int arrSize = rand() % (UPPER_LIMIT - LOWER_LIMIT + 1) + LOWER_LIMIT;
-    int mainArray[arrSize];
+    AmountAboveAverage(array, 20, average, &amountAboveAverage);
 
-    printf("Léo Mindlin - Project 3\n\n");
-    printf("Number of elements = %d\n\n", arrSize);
-    printf("Array elements: \n");
+    printf("Amount above average: %d\n", amountAboveAverage);
 
-    RandomizeArray(mainArray, arrSize);
+    return 0;
 
-    PrintArrayValues(mainArray, arrSize);
-
-    GetMinimumMaximum(mainArray, arrSize);
-
-    FindArrayAverage(mainArray, arrSize);
-
-    SortArray(mainArray, arrSize);
-
-    FindMedianOfArray(mainArray, arrSize);
 }
