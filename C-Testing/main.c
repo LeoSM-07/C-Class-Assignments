832d000b4d8705e4ad81819be2c1bbef1d7a7934
LeoSM_07 <leosmindlin@icloud.com>
2022-11-12 21:27:50 -0800

    Update

--- C-Testing/main.c
+++ C-Testing/main.c
@@ -4,55 +4,24 @@
 // 11/05/2022
 
 #include<stdio.h>
-#include<time.h>           // for time() function
-#include<stdlib.h>         // for srand() and rand() functions
 
-void rerollDice(int sum, int score);
+struct Car {
+    char make[30];
+
+    double price;
+
+    int year;
+
+    char vin[17];
+};
 
 int main (void) {
-    printf("Leo Mindlin - Project 4: Craps \n\n");
+    struct Car x = {"Chevy Impala", 12500.55, 2022, "12345678901234567"};
 
-    srand(time(NULL));
-
-    int dice1 = (rand() % 6)+1; // Dice 1 number 1-6
-    int dice2 = (rand() % 6)+1; // Dice 2 number 1-6
-    int sum = dice1 + dice2;
-
-    printf("Player Rolled %d + %d = %d \n\n", dice1, dice2, sum);
-
-    switch (sum) {
-        case 7:
-        case 11:
-            printf("Player Wins! \n");
-            break;
-        case 2:
-        case 3:
-        case 12:
-            printf("Craps! Player Looses. \n");
-            break;
-        default:
-            printf("Point is %d, game continues:\n\n", sum);
-            rerollDice(sum, sum);
-    }
-
-
-    printf("\nLeo Mindlin - End of Project 4 \n");
+    printf("Make: %s\n", x.make);
+    printf("Price: %.2f\n", x.price);
+    printf("Vin: %s\n", x.vin);
+    printf("Year: %d\n",x.year);
 
 }
 
-void rerollDice(int sum, int score) {
-    int dice1 = (rand() % 6)+1; // Dice 1 number 1-6
-    int dice2 = (rand() % 6)+1; // Dice 2 number 1-6
-    sum = dice1 + dice2;
-
-    printf("Player Rolled %d + %d = %d \n", dice1, dice2, sum);
-
-    if (sum == score) {
-        printf("\nPlayer Wins! \n");
-    } else if (sum == 7) {
-        printf("\nPlayer Looses. \n");
-    } else {
-        rerollDice(sum, score);
-    }
-
-}
